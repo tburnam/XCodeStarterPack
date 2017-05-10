81ea541e6a24f43b6242b442a4fc0e4d5a01e915
Tyler <tyler@burnamtech.com>
2017-05-10 15:31:56 -0400

    init

--- starterpackTests/starterpackTests.swift
+++ starterpackTests/starterpackTests.swift
@@ -24,6 +24,7 @@ class starterpackTests: XCTestCase {
     func testExample() {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
+        XCTAssertTrue(0 < 1)
     }
     
     func testPerformanceExample() {
